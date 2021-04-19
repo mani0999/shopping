3614361d9d918298f4122b290c5aafd1f7f16992
Manikanth Reddy <mani@Manikanths-MacBook-Pro.local>
2021-04-19 18:06:21 +0100

    update password

--- admin/include/config.php
+++ admin/include/config.php
@@ -1,7 +1,7 @@
 <?php
-define('DB_SERVER','localhost');
-define('DB_USER','root');
-define('DB_PASS' ,'');
+define('DB_SERVER','manikanth-database.cq9nxmbrwsgs.us-east-1.rds.amazonaws.com');
+define('DB_USER','admin');
+define('DB_PASS' ,'mani1234');
 define('DB_NAME', 'shopping');
 $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 // Check connection
